418f4b83447a80856e7d4ae0cc9c1e1a0b3cf00b
webrookie-zzc <zzc7026@qq.com>
2022-03-03 22:06:56 +0800

    2022.03.03 控件内容的更新

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="player">
+<!--    左侧正在播放的歌曲的信息-->
     <div class="left_info">
       <img :src="musicDetail.musicDetail[0].al.picUrl" alt="" @click="isShowLyric = !isShowLyric">
       <div class="left_name">{{musicDetail.musicDetail[0].name}}</div>
@@ -7,22 +8,54 @@
         <li v-for="item in musicDetail.musicDetail[0].ar">{{item.name}}</li>
       </ul>
     </div>
-    <audio :src="musicUrl" controls ref="audio" @timeupdate="handleTimeUpdate"></audio>
+<!--    播放控制条-->
+    <div class="play_banner_wrapper">
+<!--      上面的播放控制按钮-->
+      <div class="top_control_button_wrapper">
+        <div class="last_button"></div>
+        <div class="play_button" v-if="!isPlaySong" @click="handleClickPlayBtn"></div>
+        <div class="pause_button" v-else @click="handleClickPauseBtn"></div>
+        <div class="next_button"></div>
+      </div>
+<!--      下面的播放进度条-->
+      <div class="bottom_control_bar_wrapper">
+        <span class="have_play_time">{{songHavePlayTimeStr}}</span>
+        <div class="all_bar">
+          <div class="have_play_bar" ref="playBar"></div>
+<!--          可拖动调节进度的小球-->
+          <div class="play_ball" ref="playBall"></div>
+        </div>
+        <span class="all_time">{{songLengthStr}}</span>
+      </div>
+    </div>
+<!--    右侧播放信息-->
+    <div class="right_info">
+      <div class="SQ"></div>
+      <div class="playListInfo"></div>
+    </div>
+    <audio :src="musicUrl" ref="audio"
+           @timeupdate="handleTimeUpdate"
+           @play="startPlay(1)"
+           @pause="pausePlay"></audio>
     <teleport to="body">
       <div class="player_content" v-show="isShowLyric">
-        <div class="left_img">
-          <img :src="musicDetail.musicDetail[0].al.picUrl" alt="">
+        <div class="img_wrapper" ref="rotate">
+          <div class="left_img">
+            <img :src="musicDetail.musicDetail[0].al.picUrl" alt="">
+          </div>
         </div>
-        <!--                  评论容器-->
-        <ul class="comment_wrapper">
-          <li class="title">热门评论 >></li>
-          <li class="comment_item" v-for="item in commentList.commentList">
-            <div class="img"><img :src="item.user.avatarUrl" alt=""></div>
-            <div class="content">
-              <span style="color: #00f">{{item.user.nickname}}:</span>{{item.content}}
-            </div>
-          </li>
-        </ul>
+        <div class="out_comment_wrapper">
+          <!--                  评论容器-->
+          <ul class="comment_wrapper">
+            <li class="title">热门评论</li>
+            <li class="comment_item" v-for="item in commentList.commentList">
+              <div class="img"><img :src="item.user.avatarUrl" alt=""></div>
+              <div class="content">
+                <span style="color: #00f">{{item.user.nickname}}:</span>{{item.content}}
+              </div>
+            </li>
+          </ul>
+        </div>
         <div class="baseInfo">
           <div class="title">{{musicDetail.musicDetail[0].name}}</div>
           <div class="name_wrapper">
@@ -45,6 +78,7 @@
 import {computed, defineComponent, onBeforeMount, reactive, ref, watch} from "vue";
 import axios from "axios";
 import {useStore} from "vuex";
+import {parse} from "@vue/compiler-sfc";
 
   export default defineComponent({
     name: `Player`,
@@ -65,6 +99,8 @@ import {useStore} from "vuex";
       //监视musicUrl的变化
       watch(musicId, async newValue => {
         console.log(`检测到了ID的变化`);
+        //切歌的时候及时清除定时器，防止旋转不可控
+        clearInterval(rotateTimer)
         await getDetailMusic()
         await getLyric()
         handleLyric()
@@ -80,6 +116,9 @@ import {useStore} from "vuex";
       //歌曲详情
       let musicDetail = reactive({musicDetail: [{name: ``,al: {picUrl:``}, ar: []}]})
 
+      //当前播放歌曲的总毫米数
+      let songLength = ref<number>(0)
+
       /**
        * 获取歌曲详情
        * */
@@ -91,8 +130,9 @@ import {useStore} from "vuex";
             ids: store.state.musicId
           }
         })
-        console.log(res.data.songs);
+        console.log(res.data.songs,111);
         musicDetail.musicDetail = res.data.songs
+        songLength.value = res.data.songs[0].dt
       }
 
       /**
@@ -193,7 +233,137 @@ import {useStore} from "vuex";
        * 处理audio更新事件事件 (timeupdate)
        * */
       function handleTimeUpdate() {
+        // console.log(audio.value.currentTime);
+        songHavePlayTime.value = audio.value.currentTime
+        changePlayBar()
+        changeBallPosition()
+
         updatePlayerLyric()
+      }
+
+      /**
+       * @date 2022.3.2
+       * @brief 播放时旋转图标
+       * */
+
+      //要旋转的dom元素
+      let rotate = ref()
+
+      //元素所处于的度数
+      let picDegree:number = 0;
+
+      //旋转元素的定时器
+      let rotateTimer:number;
+
+      /**
+       * 旋转 dom 元素
+       * */
+      function rotateElement() {
+        picDegree = picDegree + 1.25;
+        if(picDegree === 360) {
+          picDegree = 0;
+        }
+        rotate.value.style.transform = `rotate(${picDegree}deg)`
+      }
+
+      /**
+       * 处理audio 开始播放的事件
+       * @param flag 0 表示从头开始播放 1 表示从中断处开始播放
+       * */
+      function startPlay(flag:number) {
+        isPlaySong.value = true
+        if(flag === 0) {picDegree = 0}
+        rotateTimer = setInterval(rotateElement, 50)
+      }
+
+      /**
+       * 处理暂停播放的事件
+       * */
+      function pausePlay() {
+        isPlaySong.value = false
+        clearInterval(rotateTimer)
+      }
+
+      /**
+       * 自定义控件的播放
+       * */
+
+      //处理后的歌曲的总时长
+      let songLengthStr = computed(() => {
+        let temp:number = songLength.value / 1000 / 60
+        temp = Number(temp.toFixed(2))
+        let minNum:number = parseInt(temp.toString())
+        let temp1 = (temp - minNum) * 60
+        let secNum:number = parseInt(temp1.toString())
+            console.log(secNum.toString().length)
+        if(secNum === 0) {return `${minNum}:00`}
+        else if(secNum.toString().length === 1) {return `${minNum}:0${secNum}`}
+            return `${minNum}:${secNum}`
+      })
+
+      //歌曲是否播放，播放的状态
+      let isPlaySong = ref<boolean>(false)
+
+      /**
+       * 处理点击播放按钮的事件
+       * */
+      function handleClickPlayBtn() {
+        audio.value.play()
+      }
+
+      /**
+       * 处理点击暂停按钮的事件
+       * */
+      function handleClickPauseBtn() {
+        audio.value.pause()
+      }
+
+      //歌曲已经播放的时长
+      let songHavePlayTime = ref<number>(0)
+
+      //处理后的字符串的已经播放的时长
+      let songHavePlayTimeStr = computed(() => {
+        let temp:number = songHavePlayTime.value / 60
+        let minNum:number = parseInt(temp.toString())
+        let temp1:number = (temp - minNum) * 60
+        temp1 = parseInt(temp1.toString())
+        let secNum:number = temp1
+        if(secNum === 0) {return `${minNum}:00`}
+        else if(secNum.toString().length === 1) {return `${minNum}:0${secNum}`}
+        return `${minNum}:${secNum}`
+      })
+
+      //播放条元素
+      let playBar = ref()
+
+      /**
+       * 调整播放条的进度
+       * */
+      function changePlayBar() {
+        let barWidth:number = audio.value.currentTime / (songLength.value / 1000) * 400
+        playBar.value.style.width = `${barWidth}px`
+      }
+
+      //可拖动小球元素
+      let playBall = ref()
+
+      //小球的相对起始的移动距离
+      let playBallLength = 0
+
+      //小球是否处于鼠标拖动的状态
+      let isMouseDown = ref<boolean>(false)
+
+      //是否显示进度小球
+      let isShowBall = ref<boolean>(false)
+
+      /**
+       * 播放改变小球的位置
+       * */
+      function changeBallPosition() {
+        if(isMouseDown.value) return
+        //仅仅当鼠标不处于鼠标拖动状态时，小球才自动随着播放进度移动
+        let leftValue:number = audio.value.currentTime / (songLength.value / 1000 ) * 400 - 5
+        playBall.value.style.left = `${leftValue}px`
       }
 
       onBeforeMount(async () => {
@@ -219,7 +389,22 @@ import {useStore} from "vuex";
         updatePlayerLyric,
         isShowLyric,
 
-        commentList
+        commentList,
+
+        rotate,
+        startPlay,
+        pausePlay,
+
+        songLength,
+        songLengthStr,
+        isPlaySong,
+        handleClickPlayBtn,
+        handleClickPauseBtn,
+        songHavePlayTime,
+        songHavePlayTimeStr,
+        playBar,
+        playBall,
+        isShowBall
       }
     }
   })
@@ -237,6 +422,7 @@ import {useStore} from "vuex";
     background-color: #fff;
     border-top: 3px solid #ccc;
     overflow: hidden;
+    display: flex;
 
     .left_info {
       width: 300px;
@@ -280,6 +466,121 @@ import {useStore} from "vuex";
     }
   }
 
+  /* 播放控件 */
+  .play_banner_wrapper {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    width: 1000px;
+    //border: 1px solid #000;
+
+    .top_control_button_wrapper {
+      width: 500px;
+      height: 40px;
+      //border: 1px solid #000;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .last_button, .play_button, .next_button, .pause_button{
+        width: 30px;
+        height: 30px;
+        //border: 1px solid #000;
+        border-radius: 50%;
+        margin-left: 20px;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+
+      .last_button {
+        background-image: url("../assets/Player/last.png");
+        background-size: 25px 25px;
+        background-position: center center;
+        background-repeat: no-repeat;
+      }
+
+      .play_button {
+        background-image: url("../assets/Player/24gf-play.png");
+        background-size: 18px 18px;
+        background-position: center center;
+        background-repeat: no-repeat;
+        transition: .5s;
+
+        &:hover {
+          background-color: rgba(0,0,0,.1);
+        }
+      }
+
+      .pause_button {
+        background-image: url("../assets/Player/pause.png");
+        background-size: 18px 18px;
+        background-position: center center;
+        background-repeat: no-repeat;
+        transition: .5s;
+
+        &:hover {
+          background-color: rgba(0,0,0,.1);
+        }
+      }
+
+      .next_button {
+        background-image: url("../assets/Player/next.png");
+        background-size: 25px 25px;
+        background-position: center center;
+        background-repeat: no-repeat;
+      }
+    }
+
+    .bottom_control_bar_wrapper {
+      width: 600px;
+      height: 10px;
+      //background-color: #666666;
+      margin-top: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      span {
+        font-size: 10px;
+        line-height: 10px;
+      }
+
+      .all_bar {
+        width: 400px;
+        height: 3px;
+        background-color: #ccc;
+        margin-left: 20px;
+        margin-right: 20px;
+        position: relative;
+
+        .have_play_bar {
+          background-color: #FFA500;
+          width: 0;
+          height: 3px;
+        }
+
+        /* 可拖动小球 */
+        .play_ball {
+          width: 10px;
+          height: 10px;
+          position: relative;
+          top: -6.5px;
+          left: -5px;
+          background-color: #fff;
+          border: 1px solid #aaa;
+          border-radius: 50%;
+
+          &:hover {
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+
   .player_content {
     position: absolute;
     left: 0;
@@ -290,20 +591,35 @@ import {useStore} from "vuex";
     background-color: #eae6e6;
     z-index: 10000;
 
-    .left_img {
+    .img_wrapper {
       width: 300px;
       height: 300px;
+      background-color: #222;
       position: absolute;
       left: 200px;
       top: 250px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+    }
+
+    .left_img {
+      width: 200px;
+      height: 200px;
+      //position: absolute;
+      //left: 200px;
+      //top: 250px;
+      border-radius: 50%;
       &:hover {
         cursor: pointer;
       }
 
       img {
-        width: 400px;
-        height: 400px;
-        border-radius: 30px;
+        width: 200px;
+        height: 200px;
+        //border-radius: 30px;
+        border-radius: 50%;
         &:hover {
           cursor: pointer;
         }
@@ -331,7 +647,7 @@ import {useStore} from "vuex";
       }
 
       .lyric_out_wrapper {
-        height: 650px;
+        height: 400px;
         margin-top: 30px;
         text-align: center;
         position: relative;
@@ -368,12 +684,25 @@ import {useStore} from "vuex";
       }
     }
 
-    .comment_wrapper {
+    .out_comment_wrapper {
+      width: 100%;
+      height: 250px;
       position: absolute;
-      top: 30px;
-      right: 30px;
-      width: 600px;
-      bottom: 0px;
+      bottom: 0;
+      overflow-x: hidden;
+      overflow-y: scroll;
+      padding: 50px;
+      &::-webkit-scrollbar {
+        display: none; /* Chrome Safari */
+      }
+    }
+
+    .comment_wrapper {
+      //position: absolute;
+      //top: 30px;
+      //right: 30px;
+      //width: 600px;
+      //bottom: 0;
       overflow-y: scroll;
       overflow-x: hidden;
       &::-webkit-scrollbar {
@@ -381,10 +710,14 @@ import {useStore} from "vuex";
       }
 
       .title {
-        font-size: 40px;
-        color: #C20C0C;
+        font-size: 20px;
+        color: #000;
         font-family: "楷体", serif;
         font-weight: bolder;
+        border-radius: 20px;
+        border:1px solid #000;
+        background-color: #ccc;
+        text-align: center;
       }
 
       .comment_item{
